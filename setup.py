4725dd3443d77a0c460cb2e13042d1965cd7492a
Hiori Kino <kino.hiori@nims.go.jp>
2020-07-08 21:42:11 +0900

    use setup.cfg

--- setup.py
+++ setup.py
@@ -1,28 +1,4 @@
-from glob import glob
-from os.path import basename
-from os.path import splitext
 
 from setuptools import setup
-from setuptools import find_packages
 
-
-def _requires_from_file(filename):
-    return open(filename).read().splitlines()
-
-
-setup(
-    name="subsMat",
-    version="0.4.0",
-    license="Apache License 2.0",
-    description="prototype substitute element calculation",
-    author="Hiori Kino",
-    url="https://github.com/nim-hrkn/substitiute_prototype",
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*/*.py')],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=_requires_from_file('requirements.txt'),
-    setup_requires=["pytest-runner"],
-    tests_require=["pytest", "pytest-cov"]
-)
+setup()
